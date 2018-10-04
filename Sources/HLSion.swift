30e1bb78485fae90aea3f8518d9b0d7ebe27143d
고현준 <linus@vlending.co.kr>
2018-10-04 11:32:36 +0900

    fixed error.

--- Sources/HLSion.swift
+++ Sources/HLSion.swift
@@ -42,7 +42,7 @@ public class HLSion {
         if HLSSessionManager.shared.assetExists(forName: name) {
             return .downloaded
         }
-        if let _ = HLSSessionManager.shared.downloadingMap.first(where: { $1.HLSion == self }) {
+        if let _ = HLSSessionManager.shared.downloadingMap.first(where: { $1 == self }) {
             return .downloading
         }
         return .notDownloaded
